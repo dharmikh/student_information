693d5d4296575485b17d8d699f34909163ad67f9
dharm <dharmikbhaliya2542@gmail.com>
2023-05-25 10:43:58 +0530

    update

--- lib/Screen/Home_Screen/Controller/HomeController.dart
+++ lib/Screen/Home_Screen/Controller/HomeController.dart
@@ -4,6 +4,9 @@ import 'package:student_information/Screen/Home_Screen/Student_Model.dart';
 class HomeController extends GetxController
 {
   RxList<StudentModel> StudentList = <StudentModel>[].obs;
+  RxList<StudentModel> StudentExamList = <StudentModel>[].obs;
+  RxList<StudentModel> StudentQuizList = <StudentModel>[].obs;
+  RxList<StudentModel> StudentHWList = <StudentModel>[].obs;
   RxInt tabIndex = 0.obs;
   Rx<StudentModel> studentInfo = StudentModel().obs;
 }
--- lib/Screen/Home_Screen/Student_type_Page.dart
+++ lib/Screen/Home_Screen/Student_type_Page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:student_information/Screen/Home_Screen/Controller/HomeController.dart';
+import 'package:student_information/Screen/Home_Screen/Student_Model.dart';
 import 'package:student_information/Utils/Api_Helper.dart';
 
 class Home_Page extends StatefulWidget {
@@ -11,7 +12,6 @@ class Home_Page extends StatefulWidget {
 }
 
 class _Home_PageState extends State<Home_Page> {
-
   HomeController homeController = Get.put(HomeController());
 
   @override
@@ -21,8 +21,13 @@ class _Home_PageState extends State<Home_Page> {
         length: 3,
         child: Scaffold(
             appBar: AppBar(
-              title: Text("Student Details"),
+              backgroundColor: Colors.blue,
+              title: Text(
+                "Student Details",
+                style: TextStyle(color: Colors.white),
+              ),
               bottom: TabBar(
+                labelColor: Colors.white,
                 onTap: (value) {
                   homeController.tabIndex.value = value;
                   print("====== $value");
@@ -45,29 +50,48 @@ class _Home_PageState extends State<Home_Page> {
                 FutureBuilder(
                   future: Api_data.api_data.Studeta(),
                   builder: (context, snapshot) {
-                    if(snapshot.hasError)
-                    {
-                      return Center(child: Text("${snapshot.error}"),);
-                    }
-                    else if(snapshot.hasData)
-                    {
+                    if (snapshot.hasError) {
+                      return Center(
+                        child: Text("${snapshot.error}"),
+                      );
+                    } else if (snapshot.hasData) {
                       homeController.StudentList.value = snapshot.data!;
+                      homeController.StudentExamList.value = [];
+                      for (StudentModel student in homeController.StudentList) {
+                        if (student.scores![0].score! > 35) {
+                          homeController.StudentExamList.add(student);
+                        }
+                      }
+                      for (StudentModel student in homeController.StudentList) {
+                        if (student.scores![1].score! > 35) {
+                          homeController.StudentExamList.add(student);
+                        }
+                      }
+                      for (StudentModel student in homeController.StudentList) {
+                        if (student.scores![2].score! > 35) {
+                          homeController.StudentExamList.add(student);
+                        }
+                      }
                       return ListView.builder(
-                        itemCount: homeController.StudentList.length,
+                        itemCount: homeController.StudentExamList.length,
                         itemBuilder: (context, index) {
                           return InkWell(
                             onTap: () {
-                              homeController.studentInfo.value = homeController.StudentList[index];
+                              homeController.studentInfo.value =
+                                  homeController.StudentExamList[index];
                               Get.toNamed('sf');
                             },
                             child: Card(
                               child: ListTile(
                                 leading: CircleAvatar(
                                   radius: 25,
-                                  backgroundImage: AssetImage("assets/image/00${homeController.StudentList[index].id! >= 100 ? homeController.StudentList[index].id : homeController.StudentList[index].id! >= 10 ? "0${homeController.StudentList[index].id}" : "00${homeController.StudentList[index].id}"}.png"),
+                                  backgroundImage: AssetImage(
+                                      "assets/image/00${homeController.StudentExamList[index].id! >= 100 ? homeController.StudentExamList[index].id : homeController.StudentExamList[index].id! >= 10 ? "0${homeController.StudentExamList[index].id}" : "00${homeController.StudentList[index].id}"}.png"),
                                 ),
-                                title: Text("${homeController.StudentList[index].name}"),
-                                subtitle: Text("${homeController.StudentList[index].scores![0].type}"),
+                                title: Text(
+                                    "${homeController.StudentExamList[index].name}"),
+                                subtitle: Text(
+                                    "${homeController.StudentExamList[index].scores![0].type}"),
                               ),
                             ),
                           );
@@ -80,29 +104,51 @@ class _Home_PageState extends State<Home_Page> {
                 FutureBuilder(
                   future: Api_data.api_data.Studeta(),
                   builder: (context, snapshot) {
-                    if(snapshot.hasError)
-                    {
-                      return Center(child: Text("${snapshot.error}"),);
-                    }
-                    else if(snapshot.hasData)
-                    {
+                    if (snapshot.hasError) {
+                      return Center(
+                        child: Text("${snapshot.error}"),
+                      );
+                    } else if (snapshot.hasData) {
                       homeController.StudentList.value = snapshot.data!;
+                      homeController.StudentQuizList.value = [];
+                      for (StudentModel student in homeController.StudentList) {
+                        if (student.scores![0].score! > 30 &&
+                            student.scores![0].score! < 35) {
+                          homeController.StudentQuizList.add(student);
+                        }
+                      }
+                      for (StudentModel student in homeController.StudentList) {
+                        if (student.scores![1].score! > 30 &&
+                            student.scores![1].score! < 35) {
+                          homeController.StudentQuizList.add(student);
+                        }
+                      }
+                      for (StudentModel student in homeController.StudentList) {
+                        if (student.scores![2].score! > 30 &&
+                            student.scores![2].score! < 35) {
+                          homeController.StudentQuizList.add(student);
+                        }
+                      }
                       return ListView.builder(
-                        itemCount: homeController.StudentList.length,
+                        itemCount: homeController.StudentQuizList.length,
                         itemBuilder: (context, index) {
                           return InkWell(
                             onTap: () {
-                              homeController.studentInfo.value = homeController.StudentList[index];
+                              homeController.studentInfo.value =
+                                  homeController.StudentQuizList[index];
                               Get.toNamed('sf');
                             },
                             child: Card(
                               child: ListTile(
                                 leading: CircleAvatar(
                                   radius: 25,
-                                  backgroundImage: AssetImage("assets/image/00${homeController.StudentList[index].id! >= 100 ? homeController.StudentList[index].id : homeController.StudentList[index].id! >= 10 ? "0${homeController.StudentList[index].id}" : "00${homeController.StudentList[index].id}"}.png"),
+                                  backgroundImage: AssetImage(
+                                      "assets/image/00${homeController.StudentQuizList[index].id! >= 100 ? homeController.StudentQuizList[index].id : homeController.StudentQuizList[index].id! >= 10 ? "0${homeController.StudentQuizList[index].id}" : "00${homeController.StudentQuizList[index].id}"}.png"),
                                 ),
-                                title: Text("${homeController.StudentList[index].name}"),
-                                subtitle: Text("${homeController.StudentList[index].scores![1].type}"),
+                                title: Text(
+                                    "${homeController.StudentQuizList[index].name}"),
+                                subtitle: Text(
+                                    "${homeController.StudentQuizList[index].scores![1].type}"),
                               ),
                             ),
                           );
@@ -115,29 +161,48 @@ class _Home_PageState extends State<Home_Page> {
                 FutureBuilder(
                   future: Api_data.api_data.Studeta(),
                   builder: (context, snapshot) {
-                    if(snapshot.hasError)
-                    {
-                      return Center(child: Text("${snapshot.error}"),);
-                    }
-                    else if(snapshot.hasData)
-                    {
+                    if (snapshot.hasError) {
+                      return Center(
+                        child: Text("${snapshot.error}"),
+                      );
+                    } else if (snapshot.hasData) {
                       homeController.StudentList.value = snapshot.data!;
+                      homeController.StudentHWList.value = [];
+                      for (StudentModel student in homeController.StudentList) {
+                        if (student.scores![0].score! < 30) {
+                          homeController.StudentHWList.add(student);
+                        }
+                      }
+                      for (StudentModel student in homeController.StudentList) {
+                        if (student.scores![1].score! < 30) {
+                          homeController.StudentHWList.add(student);
+                        }
+                      }
+                      for (StudentModel student in homeController.StudentList) {
+                        if (student.scores![2].score! < 30) {
+                          homeController.StudentHWList.add(student);
+                        }
+                      }
                       return ListView.builder(
-                        itemCount: homeController.StudentList.length,
+                        itemCount: homeController.StudentHWList.length,
                         itemBuilder: (context, index) {
                           return InkWell(
                             onTap: () {
-                              homeController.studentInfo.value = homeController.StudentList[index];
+                              homeController.studentInfo.value =
+                                  homeController.StudentHWList[index];
                               Get.toNamed('sf');
                             },
                             child: Card(
                               child: ListTile(
                                 leading: CircleAvatar(
                                   radius: 25,
-                                  backgroundImage: AssetImage("assets/image/00${homeController.StudentList[index].id! >= 100 ? homeController.StudentList[index].id : homeController.StudentList[index].id! >= 10 ? "0${homeController.StudentList[index].id}" : "00${homeController.StudentList[index].id}"}.png"),
+                                  backgroundImage: AssetImage(
+                                      "assets/image/00${homeController.StudentHWList[index].id! >= 100 ? homeController.StudentHWList[index].id : homeController.StudentHWList[index].id! >= 10 ? "0${homeController.StudentHWList[index].id}" : "00${homeController.StudentHWList[index].id}"}.png"),
                                 ),
-                                title: Text("${homeController.StudentList[index].name}"),
-                                subtitle: Text("${homeController.StudentList[index].scores![2].type}"),
+                                title: Text(
+                                    "${homeController.StudentHWList[index].name}"),
+                                subtitle: Text(
+                                    "${homeController.StudentHWList[index].scores![2].type}"),
                               ),
                             ),
                           );
@@ -148,8 +213,7 @@ class _Home_PageState extends State<Home_Page> {
                   },
                 ),
               ],
-            )
-        ),
+            )),
       ),
     );
   }
